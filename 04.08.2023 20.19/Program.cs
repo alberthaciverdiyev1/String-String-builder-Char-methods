ee5d5dc8dff1e1b9c4422912d7b9efa63b134cae
Alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-04-09 23:45:44 +0400

    Task

---
+++ 04.08.2023 20.19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _04._08._2023_20._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine(Correct(sentence));
+        }
+        public static string Correct(string sentence)
+        {
+            string CorrectSentence = sentence;
+            string[] words = CorrectSentence.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
